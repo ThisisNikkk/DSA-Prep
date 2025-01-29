2d289351166629e7a369e9bea22eddfbe5a9601f
Nikhil Siwan <developer.nikk@gmail.com>
2025-01-29 15:13:31 +0530

    Pattern

---
+++ Patterns/p7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number of rows: " << endl;
+    cin >> n;
+    cout<<endl;
+
+    for(int i = 0; i <n ; i++){
+        //space
+        for(int j =0; j < i; j++){
+            cout<< " ";
+        }
+        for(int j =0; j < 2*n-(2*i+1); j++){
+            cout << "*";
+        }
+        for(int j =0; j < i; j++){
+            cout << " ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
